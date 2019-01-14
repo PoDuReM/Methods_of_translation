d7c3b30dd055c63d7bc7f147adc58dbf15d171e5
Tina Smirnova <tinanight98@gmail.com>
2019-01-14 23:29:27 +0300

    Demo commit

---
+++ parser-generator/CMakeFiles/parser-generator.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tina/Documents/ITMO/CT/MT/parser-generator/generated/TLexer.cpp" "/Users/tina/Documents/ITMO/CT/MT/parser-generator/CMakeFiles/parser-generator.dir/generated/TLexer.cpp.o"
+  "/Users/tina/Documents/ITMO/CT/MT/parser-generator/generated/TParser.cpp" "/Users/tina/Documents/ITMO/CT/MT/parser-generator/CMakeFiles/parser-generator.dir/generated/TParser.cpp.o"
+  "/Users/tina/Documents/ITMO/CT/MT/parser-generator/generated/TParserBaseListener.cpp" "/Users/tina/Documents/ITMO/CT/MT/parser-generator/CMakeFiles/parser-generator.dir/generated/TParserBaseListener.cpp.o"
+  "/Users/tina/Documents/ITMO/CT/MT/parser-generator/generated/TParserBaseVisitor.cpp" "/Users/tina/Documents/ITMO/CT/MT/parser-generator/CMakeFiles/parser-generator.dir/generated/TParserBaseVisitor.cpp.o"
+  "/Users/tina/Documents/ITMO/CT/MT/parser-generator/generated/TParserListener.cpp" "/Users/tina/Documents/ITMO/CT/MT/parser-generator/CMakeFiles/parser-generator.dir/generated/TParserListener.cpp.o"
+  "/Users/tina/Documents/ITMO/CT/MT/parser-generator/generated/TParserVisitor.cpp" "/Users/tina/Documents/ITMO/CT/MT/parser-generator/CMakeFiles/parser-generator.dir/generated/TParserVisitor.cpp.o"
+  "/Users/tina/Documents/ITMO/CT/MT/parser-generator/main.cpp" "/Users/tina/Documents/ITMO/CT/MT/parser-generator/CMakeFiles/parser-generator.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include/antlr4-runtime"
+  "generated"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ parser-generator/CMakeFiles/parser-generator.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parser-generator.dir/main.cpp.o"
+  "CMakeFiles/parser-generator.dir/generated/TLexer.cpp.o"
+  "CMakeFiles/parser-generator.dir/generated/TParser.cpp.o"
+  "CMakeFiles/parser-generator.dir/generated/TParserBaseListener.cpp.o"
+  "CMakeFiles/parser-generator.dir/generated/TParserBaseVisitor.cpp.o"
+  "CMakeFiles/parser-generator.dir/generated/TParserListener.cpp.o"
+  "CMakeFiles/parser-generator.dir/generated/TParserVisitor.cpp.o"
+  "parser-generator.pdb"
+  "parser-generator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parser-generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
